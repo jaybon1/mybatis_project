c88408bdcef0c64cecb22f49bf8c2efb1081b11e
jaybon1 <ponggas2@gmail.com>
2023-03-08 02:59:28 +0900

    로그 장착

--- src/main/java/com/example/mybatis_project/todo/controller/MyBatisTodoApiController.java
+++ src/main/java/com/example/mybatis_project/todo/controller/MyBatisTodoApiController.java
@@ -3,9 +3,11 @@ package com.example.mybatis_project.todo.controller;
 import com.example.mybatis_project.todo.dto.TodoDTO;
 import com.example.mybatis_project.todo.service.MyBatisTodoApiService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -15,17 +17,19 @@ public class MyBatisTodoApiController {
 
     @PostMapping("/todoList")
     public HttpEntity<?> insert(@RequestBody TodoDTO.ReqBasic reqDto) {
-        System.out.println(reqDto.getContent());
+        log.info(reqDto.getContent() + "를 등록합니다.");
         return myBatisTodoApiService.insert(reqDto);
     }
 
     @PutMapping("/todoList")
     public HttpEntity<?> update(@RequestBody TodoDTO.ReqUpdate reqDto) {
+        log.info(reqDto.getIdx() + "번 할 일을 수정합니다.");
         return myBatisTodoApiService.updateDoneYn(reqDto);
     }
 
     @DeleteMapping("/todoList")
     public HttpEntity<?> delete(@RequestBody TodoDTO.ReqDelete reqDto) {
+        log.error(reqDto.getIdx() + "번 할 일을 삭제합니다.");
         return myBatisTodoApiService.updateDeleteYn(reqDto);
     }
 
